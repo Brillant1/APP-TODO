b2306cd7906cae72dd48f59c86084a9d619e27b0
Esaie TCHAGNONSI <esaietchagnonsi@gmail.com>
2024-05-31 13:47:33 +0100

    correction pwa config

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: '#7451eb',
         icons: [
           {
-            "src": "/android-chrome-192x192.png",
+            "src": "/icons/android-chrome-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "/android-chrome-512x512.png",
+            "src": "/icons/android-chrome-51x512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
