c42fe36c99dd80d7b4f6041af55a9a8a7520ddaf
Esaie TCHAGNONSI <esaietchagnonsi@gmail.com>
2024-05-31 10:48:05 +0100

    Add pwa configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Application de gestion de tâche',
+        short_name: 'TODO APP',
+        description: 'Permet de suivre ces tâches quotidiennes',
+        theme_color: '#7451eb',
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
